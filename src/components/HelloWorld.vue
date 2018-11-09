5d08f691e04777c2c5d1cba8fd371d27f35e4b39
Devin_Chen <ChenZhanhui1992@outlook.com>
2018-11-09 18:03:32 +0800

    初提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <div>你猜我多大</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+@import '../../static/scss.scss';
+.hello{
+  height: px2Rem(200);
+  width: px2Rem(300);
+  font-size: px2Rem(24);
+  background-color: blue;
+}
+</style>
